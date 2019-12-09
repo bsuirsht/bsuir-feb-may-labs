40c7589dd67b2a5651a0de472539ce3a7ebba4d3
bsuirsht <igoodlike2@gmail.com>
2019-12-09 13:35:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My lab 4 solution is [here](https://github.com/bsuirsht/bsuir-feb-may-labs)
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./index.html"
+    }), new Dotenv()],
+    module: {
+        rules: [
+            {
+                test: /\.s?[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(html)$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: "file-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }
+        ],
+    }
+};
